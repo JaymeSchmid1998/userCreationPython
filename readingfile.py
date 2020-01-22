0c76ef7201a93765733b26465a0bc5528b8ae2d8
jayme schmid <jayme.schmid@gmail.com>
2020-01-22 06:34:25 +0000

    added in nfc funcationality,led functionality and also deleting of the old file

--- readingfile.py
+++ readingfile.py
@@ -1,9 +1,19 @@
 from pathlib import *
+import time
+import RPi.GPIO as GPIO
+import nfc
+import ndef
+import threading
+import nfc.snep
+
 # coding: utf-8
 my_file=Path('UserCreation.txt')
 mylines = []
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22,GPIO.OUT)
 # this checks if the file exists
 if my_file.exists():
+    clf = nfc.ContactlessFrontend('')
 #if it does hthen open it and append the lines to the array
     objText = open('UserCreation.txt', "r")
     for line in objText:
@@ -19,8 +29,25 @@ if my_file.exists():
     mytext1 = mytext.replace("AuthCode: ", "")
 
     print(mytext1)
+    GPIO.output(22,1)
     
     objText.close()
+    my_file.unlink()
     print('file found')
+    try:
+        clf = nfc.ContactlessFrontend('tty:USB0:pn532')
+        clf.connect(llcp={})
+        def on_connect(llc):
+            threading.Thread(target=llc.run).start(); return False
+        llc = clf.connect(llcp={'on-connect': on_connect})
+        print (llc)
+        snep = nfc.snep.SnepClient(llc)
+        snep.put_records([ndef.TextRecord(mytext1, "de")])
+        GPIO.output(22,0)
+        clf.close()
+    except: 
+        clf.close()
+        
 else:
     print('no file found')
+    GPIO.output(22,0)
